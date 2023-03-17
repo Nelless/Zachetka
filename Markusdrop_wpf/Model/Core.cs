37111edbfdb4bd156ac797137eca6bc6dc12a558
emk1online <emk1online@gmail.com>
2023-03-17 13:34:05 +0500

    Добавьте файлы проекта.

---
+++ Markusdrop_wpf/Model/Core.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markusdrop_wpf.Model
+{
+    internal class Core
+    {
+    }
+}
